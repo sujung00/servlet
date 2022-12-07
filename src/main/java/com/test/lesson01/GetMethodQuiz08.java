75bda95245b8eb482d89abd97328e6c6c3958b8e
sujung00 <rlatnwjdirin@naver.com>
2022-12-07 17:36:39 +0900

    초기 환경 세팅

---
+++ src/main/java/com/test/lesson01/GetMethodQuiz08.java
@@ -0,0 +1,52 @@
+package com.test.lesson01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/lesson01/quiz08")
+public class GetMethodQuiz08 extends HttpServlet{
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		
+		String search = request.getParameter("keyword");
+		
+		PrintWriter out = response.getWriter();
+		
+		List<String> list = new ArrayList<>(Arrays.asList(
+		        "강남역 최고 맛집 소개 합니다.", 
+		        "오늘 기분 좋은 일이 있었네요.", 
+		        "역시 맛집 데이트가 제일 좋네요.", 
+		        "집에 가는 길에 동네 맛집 가서 안주 사갑니다.",
+		        "자축 저 오늘 생일 이에요."));
+		
+		out.print("<html><head><title>Quiz08</title></head><body>");
+		
+		Iterator iter = list.iterator();
+		while(iter.hasNext()) {
+			String sentence = (String) iter.next();
+			if(sentence.contains(search)) {
+				out.print(sentence.replace(search, "<b>"+search+"</b>") + "<br>");
+			}
+			
+//			if(sentence.contains(search)) {
+//				String[] words = sentence.split(search);
+//				out.print(words[0] + "<b>" + search + "</b>" + words[1] + "<br>");
+//			}
+		}
+		
+		out.print("</body></html>");
+	}
+
+}
---
+++ src/main/java/com/test/lesson01/UrlMappingQuiz03.java
@@ -0,0 +1,30 @@
+package com.test.lesson01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/lesson01/quiz03")
+public class UrlMappingQuiz03 extends HttpServlet{
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		
+		Date now = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		
+		PrintWriter out = response.getWriter();
+		out.print("<html><head><title>Quiz03</title></head><body>");
+		out.print("<h2>[단독] 고양이가 야옹해</h2>");
+		out.print("기사 입력 시간: " + sdf.format(now) + "<hr>끝");
+		out.print("</body></html>");
+	}
+}
